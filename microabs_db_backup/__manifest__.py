bae3b6ae51e24c7943e1761ce0b6f8d58947e765
smrithi.s <smrithi.s@brainvire.com>
2021-07-21 09:09:51 +0530

    [IMP] Database Backup Feature

---
+++ microabs_db_backup/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Microabs - DB Backup',
+    'version': '12.0.1.0.0',
+    'summary': 'Microabs - DB Backup',
+    'description': 'This module consists of DB Backup',
+    'category': 'Website',
+    'author': 'Howk-i ERP Zone - Simplified',
+    'support': 'howk.i.erp@gmail.com',
+    'website': 'http://erp.howk-i.com/',
+    'license': 'AGPL-3',
+    'depends': ['web'],
+    'data': [
+             'views/backup.xml',
+             ],
+    'qweb': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ microabs_db_backup/controllers/main.py
@@ -0,0 +1,47 @@
+import odoo
+from odoo import http
+from odoo.http import request
+import jinja2
+import json
+import os
+import sys
+from odoo.addons.web.controllers.main import Database
+
+DBNAME_PATTERN = '^[a-zA-Z0-9][a-zA-Z0-9_.-]+$'
+db_monodb = http.db_monodb
+
+if hasattr(sys, 'frozen'):
+    # When running on compiled windows binary, we don't have access to package loader.
+    path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'views'))
+    loader = jinja2.FileSystemLoader(path)
+else:
+    loader = jinja2.PackageLoader('odoo.microabs_addons.microabs_db_backup', "views")
+
+env = jinja2.Environment(loader=loader, autoescape=True)
+env.filters["json"] = json.dumps
+
+
+class BackupDatabase(http.Controller):
+
+    def _render_backup_template(self, **d):
+        d.setdefault('manage', True)
+        d['insecure'] = odoo.tools.config.verify_admin_password('admin')
+        d['list_db'] = odoo.tools.config['list_db']
+        d['langs'] = odoo.service.db.exp_list_lang()
+        d['countries'] = odoo.service.db.exp_list_countries()
+        d['pattern'] = DBNAME_PATTERN
+        # databases list
+        d['databases'] = []
+        try:
+            d['databases'] = http.db_list()
+            d['incompatible_databases'] = odoo.service.db.list_db_incompatible(d['databases'])
+        except odoo.exceptions.AccessDenied:
+            monodb = db_monodb()
+            if monodb:
+                d['databases'] = [monodb]
+        return env.get_template("database_backup.html").render(d)
+
+    @http.route('/web/database/dbbackup', type='http', auth="none")
+    def dbbackup(self, **kw):
+        request._cr = None
+        return self._render_backup_template()
