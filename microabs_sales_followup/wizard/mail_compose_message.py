015f617709f3ecf62c9baacf673a494e21c5ba7f
smrithi.s <smrithi.suresh26@gmail.com>
2021-09-26 22:12:33 +0530

    [IMP] Added two fullstops and regards address

--- microabs_sales_followup/wizard/mail_compose_message.py
+++ microabs_sales_followup/wizard/mail_compose_message.py
@@ -14,10 +14,11 @@ class SaleFollowup(models.Model):
         # invoice_ids = self.env['account.invoice'].browse(self._context.get('active_ids'))
         email_to = sales_ids.partner_id.so_email_to
         email_cc = sales_ids.partner_id.so_email_cc
-        email_subject = 'Order Request Form - %s - %s - %s - %s' % (sales_ids.partner_id.name,
-                                                                    sales_ids.partner_id.city,
-                                                                    sales_ids.po_no,
-                                                                    sales_ids.po_date.strftime("%d-%m-%Y") if sales_ids.po_date else " ")
+        email_subject = 'Order Request Form - %s - %s - %s - %s.' % (sales_ids.partner_id.name,
+                                                                     sales_ids.partner_id.city,
+                                                                     sales_ids.po_no,
+                                                                     sales_ids.po_date.strftime(
+                                                                         "%d-%m-%Y") if sales_ids.po_date else " ")
         attachments = self.env['ir.attachment'].search(
             [('res_id', '=', sales_ids.id), ('res_model', '=', 'sale.order')]).ids
         res.update({
@@ -32,26 +33,30 @@ class SaleFollowup(models.Model):
             title = sales_ids.partner_id.title.name if sales_ids.partner_id.title else inv.title.name
             partner += title + " " + inv.name + "/"
         partner = partner[:-1] if partner else "Customer"
-        message = "<p>Dear %s,</p>" % partner
+        message = "<p>Dear %s:</p>" % partner
         message += "<p>Greetings of the day!<br/></p>"
         message += "<p>Pl. find the enclosed PO. %s dtd %s received from %s on %s " \
-                   "Our corresponding Order Request Number is %s <br/>Kindly acknowledge the receipt of this order.</p>" \
+                   "Our corresponding Order Request Number is %s .<br/>" \
+                   "Kindly acknowledge the receipt of this order.</p>" \
                    % (sales_ids.po_no, sales_ids.po_date.strftime("%d-%m-%Y") if sales_ids.po_date else " ",
                       sales_ids.partner_id.name if sales_ids.partner_id.name else " ",
-                      sales_ids.po_received_date.strftime("%d-%m-%Y") if sales_ids.po_received_date else " ", sales_ids.name)
+                      sales_ids.po_received_date.strftime("%d-%m-%Y") if sales_ids.po_received_date else " ",
+                      sales_ids.name)
         message += "<p>The delivery week required by the customer is mentioned in the enclosed Order Request form.<br/>"
         message += "Pl. let us know your Delivery week and packing list.<br/>"
         message += "Kindly let us know in case of any queries.</p>"
         message += "<p>Regards,<br/>"
-        message += "<b>ERP Team.</b><br/>"
-        message += "%s <br/>" % sales_ids.company_id.name
-        message += "%s, <br/>" % sales_ids.company_id.street if sales_ids.company_id.street else ""
-        message += "%s, <br/>" % sales_ids.company_id.street2 if sales_ids.company_id.street2 else ""
-        message += "%s, <br/>" % sales_ids.company_id.city if sales_ids.company_id.city else ""
-        message += "%s, <br/>" % sales_ids.company_id.state_id.name if sales_ids.company_id.state_id else ""
-        message += "%s - %s. </p>" % (
-            sales_ids.company_id.country_id.name if sales_ids.company_id.country_id else "",
-            sales_ids.company_id.zip if sales_ids.company_id.zip else "")
+        message += "ERP Team.<br/>"
+        message += "Micro Abrasive Technologies,<br/>"
+        message += "Chennai.</p>"
+        # message += "%s <br/>" % sales_ids.company_id.name
+        # message += "%s, <br/>" % sales_ids.company_id.street if sales_ids.company_id.street else ""
+        # message += "%s, <br/>" % sales_ids.company_id.street2 if sales_ids.company_id.street2 else ""
+        # message += "%s, <br/>" % sales_ids.company_id.city if sales_ids.company_id.city else ""
+        # message += "%s, <br/>" % sales_ids.company_id.state_id.name if sales_ids.company_id.state_id else ""
+        # message += "%s - %s. </p>" % (
+        #     sales_ids.company_id.country_id.name if sales_ids.company_id.country_id else "",
+        #     sales_ids.company_id.zip if sales_ids.company_id.zip else "")
         res.update({
             'email_body': message,
             'sale_id': sales_ids.id
